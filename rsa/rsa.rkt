0ab90a2cfb4abfead7080bf76f4f9b6a11090936
karturbis <vulkanera42@gmx.ch>
2024-04-26 16:37:39 +0200

    implement better algorithm for b^e modulo(m)

--- rsa/rsa.rkt
+++ rsa/rsa.rkt
@@ -1,16 +1,46 @@
 #lang racket
-;(require "keygen.rkt"); import the keys from keygen.txt
+(require "keygen.rkt"); import the keys from keygen.txt
 
 (define (encrypt e N message)
-  (modulo
-   (expt message e)
-   N
-  )
+  (mod-exp message e N)
 )
 
 (define (decrypt d N chiffre)
-  (modulo
-   (expt chiffre d)
-   N
+  (mod-exp chiffre d N)
+)
+
+
+(define (integer->bitlist a)
+  (reverse
+   (string->list
+    (number->string a 2)
+   )
   )
+)
+
+(define (mod-exp b e m)
+  (mod-exp-inner 1 b (integer->bitlist e) m 0)
+)
+
+(define (mod-exp-inner r x e m i)
+  (if (>= i (length e))
+      r
+      (mod-exp-inner
+       (modulo
+        (* r (expt
+              x
+              (- (char->integer (list-ref e i)) 48); to convert from ascii number to real number
+             )
+         )
+        m; the modulus
+        ); this is the r parameter of recursive call
+       (modulo
+        (expt x 2)
+        m
+       )
+       e
+       m
+       (+ i 1)
+      )
+   )
 )
